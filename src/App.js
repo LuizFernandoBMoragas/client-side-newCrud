89768133d20dd2ff32f0a838d0804adb9963b177
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-04-17 21:03:29 +0100

    sending our object to back-end and succefuly database conection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Axios from "axios";
 import "./App.css";
 
 function App() {
@@ -8,22 +9,57 @@ function App() {
   const [foodOrigin, setFoodOrigin] = useState("");
   const [foodDrink, setFoodDrink] = useState("");
 
+  const addToMenu = () => {
+    Axios.post("http://localhost:8080/insert", {
+      foodName: foodName,
+      days: days,
+      foodType: foodType,
+      foodOrigin: foodOrigin,
+      foodDrink: foodDrink,
+    });
+  };
+
   return (
     <div className="App">
       <h1>Food Menu Model</h1>
 
       <label>Food Name:</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setFoodName(event.target.value);
+        }}
+      />
       <label>Expiration Date(days after made):</label>
-      <input type="number" />
+      <input
+        type="number"
+        onChange={(event) => {
+          setDays(event.target.value);
+        }}
+      />
       <label>Type Of Food(Sweet or Salty):</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setFoodType(event.target.value);
+        }}
+      />
       <label>Country Of Origin:</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setFoodOrigin(event.target.value);
+        }}
+      />
       <label>Best Choose To Drink With:</label>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(event) => {
+          setFoodDrink(event.target.value);
+        }}
+      />
 
-      <button>Add to Menu Model</button>
+      <button onClick={addToMenu}>Add to Menu Model</button>
     </div>
   );
 }
