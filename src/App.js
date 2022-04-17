aa192a926567df40d2ee6cd3ce7b63cc48a4e7b6
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-04-17 19:58:03 +0100

    react enviroment done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>CRUD App with MERN</h1>
+    </div>
+  );
 }
 
 export default App;
